55a86856fb7fb5eb5f2573df277de519fdd8cd03
joser420 <0815231022@s.tusd1.org>
2026-03-02 15:46:47 +0000

    hi

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("script started");
+
+//variable to track
+let clicks = 1;
+
+//store click conter
+let clickDisplay = document.getElementById("click-display");
+
+// set text for click 
+clickDisplay.innerText = "clicks:" + clicks;
