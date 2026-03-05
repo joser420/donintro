73614a45596ecc46da865324c115df7848f11575
joser420 <0815231022@s.tusd1.org>
2026-03-05 15:40:34 +0000

    gctyt

--- script.js
+++ script.js
@@ -2,22 +2,28 @@ console.log("script started");
 
 //variable to track
 let clicks = 0;
-
+//value
+let cliclValue = 1;
 //store click conter
 let clickDisplay = document.getElementById("click-display");
+//level display
+let levelDisplay = document.getElementById("level-display")
 
 // set text for click 
 clickDisplay.innerText = "clicks:" + clicks;
 //each time click called
 function handleclick(){
     console.log("click");
-}
- // Called each time the image is clicked
-    function handleClick() {
-        console.log("click");
-        // Add one to click count
-        clicks = clicks + 1;
-        // Update click count display h1
-        countDisplay.innerText = "Clicks: " + clicks;
+    // Add one to click count
+    clicks = clicks + cliclValue;
+    // Update click co1unt display h1
+    clickDisplay.innerText = "Clicks: " + clicks;
+    
+    if (clicks == 10){
+        alert("congrats!");
+        levelDisplay.innerText = "level 2";
+        cliclValue = 2;
     }
-    if 
+}
+
+        
